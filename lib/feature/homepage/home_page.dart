e07e43b803b6f3c938b9687a50e044cea9146450
skyphantomhz <skyphantomhz@gmail.com>
2021-03-16 07:17:24 +0700

    term develop

--- lib/feature/homepage/home_page.dart
+++ lib/feature/homepage/home_page.dart
@@ -18,7 +18,7 @@ class _HomePageState extends State<HomePage> {
           children: [
             Container(
               color: Colors.white,
-              padding: EdgeInsets.only(left: 16, right: 16),
+              padding: EdgeInsets.only(left: 16, right: 16, bottom: 16),
               child: Row(children: [
                 Container(
                     width: 34,
@@ -47,6 +47,73 @@ class _HomePageState extends State<HomePage> {
                   ),
                 ),
               ]),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Container(
+                    padding: EdgeInsets.only(left: 16, right: 16),
+                    child: Column(
+                      children: [
+                        Stack(
+                          children: [
+                            Image.network(
+                              "https://i.redd.it/tjg86bw6bou21.jpg",
+                            ),
+                            Positioned(
+                              bottom: 4,
+                              right: 4,
+                              child: Container(
+                                padding: EdgeInsets.all(4),
+                                color: Colors.black45,
+                                child: Text(
+                                  "1:23:00",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 8, bottom: 8),
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 40,
+                                height: 40,
+                                margin: EdgeInsets.only(right: 8),
+                                child: CircleAvatar(
+                                  backgroundImage: NetworkImage(
+                                      "https://image.freepik.com/free-vector/cartoon-monster-face-avatar-halloween-monster_6996-1164.jpg"),
+                                ),
+                              ),
+                              Expanded(
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      "Relaxing Music with Birds Singing - Beautiful Piano Music & Guitar Musi...",
+                                      style: TextStyle(),
+                                    ),
+                                    Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: Text(
+                                        "Soothing Relaxation . 251M views",
+                                        textAlign: TextAlign.left,
+                                        style: TextStyle(),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
             )
           ],
         ),
